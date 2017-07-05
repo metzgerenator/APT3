53ecbafd621e008941b3ae693ea28c5ad55368b1
metzgerenator <metzgerenator@gmail.com>
2017-07-05 19:21:01 +0200

    added static embedded tableview

--- Apt/PropertyDetailsViewController.swift
+++ Apt/PropertyDetailsViewController.swift
@@ -14,11 +14,9 @@ class PropertyDetailsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationController?.title = "ADD DETAILS"
         
-        print("here is property name \(propertyName)")
-        
-        
-
+    
         // Do any additional setup after loading the view.
     }
 
@@ -28,14 +26,6 @@ class PropertyDetailsViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  
 
 }
