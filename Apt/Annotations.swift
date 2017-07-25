a43ebf5eedf9e299f0a6b981889992f433bfb7d7
metzgerenator <metzgerenator@gmail.com>
2017-07-25 21:35:53 +0200

    added custom unit's class for annotation

--- Apt/Annotations.swift
+++ Apt/Annotations.swift
@@ -17,3 +17,28 @@ struct MapPoints {
 }
 
 
+class Units: NSObject, MKAnnotation {
+    
+  
+    let title: String?
+    let subtitle: String?
+    let coordinate: CLLocationCoordinate2D
+    
+    init(unit: Apartment, latitude: Double, longitude: Double){
+        
+       
+         let title = unit.apartmentName ?? "no title"
+        let addressString = unit.location?.address ?? "no addresss"
+        
+        self.title = title
+        self.subtitle = addressString
+        self.coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: latitude)
+        
+        super.init()
+        
+    }
+    
+    
+}
+
+
